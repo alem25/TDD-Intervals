5ee4784b8b44f331e6048ddf46a11f7907e2a355
alem25 <alem2518@hotmail.com>
2021-04-25 11:52:36 +0200

    v4 RED includeOtherInterval always return true...

--- src/test/java/usantatecla/TwoOpenIntervalsTest.java
+++ src/test/java/usantatecla/TwoOpenIntervalsTest.java
@@ -30,4 +30,11 @@ public class TwoOpenIntervalsTest {
         assertTrue(this.firstOpenInterval.includeOtherInterval(this.secondOpenInterval));
     }
 
+    @Test
+    public void givenTwoIntervalsWhenFirstIncludeSecondIntervalThenFalse() {
+        this.firstOpenInterval = new IntervalBuilder().open(this.point1.getEquals()).open(this.point2.getEquals()).build();
+        this.secondOpenInterval = new IntervalBuilder().open(this.point3.getEquals()).open(this.point4.getEquals()).build();
+        assertFalse(this.firstOpenInterval.includeOtherInterval(this.secondOpenInterval));
+    }
+
 }
